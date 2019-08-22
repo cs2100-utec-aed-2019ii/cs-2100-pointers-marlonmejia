c1fac46a8f32709748c17d023a1f941c23e40c1e
marlonmejia <marlon.mejia@utec.edu.pe>
2019-08-22 17:00:55 -0500

    avance_a
    ..

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#define SIZE 10
+using namespace std;
 
 /** ejemplo **/
-void ejercicio_x (int, int) {
-    return;
+void ejercicio_1 () {
+    int array[SIZE];
+    int *ptr = array;
+    for (int i = 0; i < SIZE; i++) {
+        *(ptr + i) = i + 1;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        cout << array[i] << " ";
+    }
+}
+void function_swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
 }
 
-int main (int, char * []){
+int main (){
+    //Respuesta de la 1:
+    //1 2 3 4 5 6 7 8 9 10
+    //ejercicio_1();
+    
+    //Respuesta de la 2:
+    //7 5 10
+    int a=5,b=10,c=7;
+    function_swap(a,b);
+    function_swap(a,c);
+    cout << a << " " << b <<" "<< c << endl;
+
+
+
     return 1;
 }
